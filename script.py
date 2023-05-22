b9d1d67f879e5ed0262acbbbedfdda90aa5a5438
cvoneul <cvoneul@gmail.com>
2023-05-22 16:21:16 -0700

    installed pip/pytube

--- script.py
+++ script.py
@@ -1,20 +1,20 @@
-try:
+import os
+os.system("curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py")
+os.system("python get-pip.py")
+os.system("pip install pytube")
 
-    from pytube import YouTube
-    import os
+from pytube import YouTube
 
-    yt = YouTube( str(input("Enter the URL of the youtube video you want downloaded : \n")))
+yt = YouTube( str(input("Enter the URL of the youtube video you want downloaded : \n")))
 
-    video = yt.streams.filter(only_audio=True).first()
+video = yt.streams.filter(only_audio=True).first()
 
-    destination =  "."
+destination =  "."
 
-    out_file = video.download(output_path=destination)
+out_file = video.download(output_path=destination)
 
-    base, ext = os.path.splitext(out_file)
-    new_file = base + '.mp3'
-    os.rename(out_file, new_file)
+base, ext = os.path.splitext(out_file)
+new_file = base + '.mp3'
+os.rename(out_file, new_file)
 
-    print('Successfully downloaded')
-except:
-    print("error")
+print('Successfully downloaded')
