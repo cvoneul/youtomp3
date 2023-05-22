c64782e5d02b396caaf34e94442219f2aa954d6d
cvoneul <cvoneul@gmail.com>
2023-05-22 16:01:08 -0700

    added script

--- script.py
+++ script.py
@@ -1,1 +1,16 @@
-print('sup foo')
+from pytube import YouTube
+import os
+
+yt = YouTube( str(input("Enter the URL of the youtube video you want downloaded : \n")))
+
+video = yt.streams.filter(only_audio=True).first()
+
+destination =  "."
+
+out_file = video.download(output_path=destination)
+
+base, ext = os.path.splitext(out_file)
+new_file = base + '.mp3'
+os.rename(out_file, new_file)
+
+print('Successfully downloaded')
