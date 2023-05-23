ffc70f6cbc2af8dfc8e2a93561fb770124e18889
cvoneul <cvoneul@gmail.com>
2023-05-22 18:29:44 -0700

    attemp2

--- script.py
+++ script.py
@@ -1,14 +1,12 @@
 import os
-os.system("pip unistall pytube")
-os.system("pip unistall pytube3")
-os.system("python -m pip install git+https://github.com/nficano/pytube")
+import pytube
 from pytube import YouTube
 
 yt = YouTube( str(input("Enter the URL of the youtube video you want downloaded : \n")))
 
 video = yt.streams.filter(only_audio=True).first()
 
-destination =  "C:\\19095\\Downloads\\"
+destination =  "C:\\Users\\19095\\Downloads\\"
 
 out_file = video.download(output_path=destination)
 
